bc1a199fab1fa82f8b054f4259592cfe176f764b
Léandre <leandre.colard@gmail.com>
2024-12-10 16:40:38 +0100

    comments md5

--- src/hash/Md5.java
+++ src/hash/Md5.java
@@ -8,6 +8,10 @@ public class Md5 {
     private static final Charset UTF_8 = StandardCharsets.UTF_8;
     private static final String OUTPUT_FORMAT = "%-20s:%s";
 
+    /*
+     * tries the selected method and digests with the selected method
+     */
+
     private static byte[] digest(byte[] input) {
         MessageDigest md;
         try {
@@ -17,10 +21,13 @@ public class Md5 {
             throw new IllegalArgumentException(e);
         }
         byte[] result = md.digest(input);
+        System.out.println(result);
         return result;
     }
 
-    // transforms bytes to hex, uses stringbuilder
+    /*
+     * transforms bytes to hex, uses stringbuilder
+     */
 
     private static String bytesToHex(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
@@ -29,6 +36,11 @@ public class Md5 {
         }
         return sb.toString();
     }
+
+
+    /*
+     * main caller, currently just outputs stuff in the console
+     */
 
     public static void formatter(String message) {
 
@@ -46,7 +58,9 @@ public class Md5 {
 
     }
 
-    // caller to adapt if needed
+    /*
+     * just main being main...
+     */
 
     public static void main(String[] args) {
         System.out.println("--------------------");
