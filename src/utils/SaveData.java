8e87caf0b068057340c9a20ef99b026bc0c55f93
Mizer3 <91741197+Mizer3@users.noreply.github.com>
2024-12-11 11:43:52 +0100

    save csv file

---
+++ src/utils/SaveData.java
@@ -0,0 +1,36 @@
+package utils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SaveData {
+
+    /**
+     * Save the data to a file
+     */
+
+    // Store the data with no key
+    public static void saveData(String name, String password, String method) {
+        saveData(name, password, method, "no key");
+    }
+
+    // Store the data with a key
+    public static void saveData(String name, String password, String method, String key) {
+        File file = new File("data.csv");
+        boolean fileExists = file.exists();
+
+        // Store the data in the file
+        try (FileWriter writer = new FileWriter(file, true)) {
+            // If the file does not exist, write the header
+            if (!fileExists) {
+                writer.write("Password Store\n");
+            }
+            // Write the data
+            writer.write(name + " : " + password + ", " + method + ", " + key + ";" + "\n");
+            System.out.println("Data saved successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
