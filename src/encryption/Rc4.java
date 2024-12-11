307ca75802931eaab5e3e050b8fb9d98953b5cbc
Léandre <leandre.colard@gmail.com>
2024-12-11 16:20:00 +0100

    fix structure of RC4

--- src/encryption/Rc4.java
+++ src/encryption/Rc4.java
@@ -8,32 +8,32 @@ import utils.Lfsr;
 
 public class Rc4 {
 
-    /*
+    public static void main(String[] args) {
+        System.out.println(Rc4Encrypt("oui bonsoir !", "une bonne seed en fait"));
+        
+    }
+
+    /**
      * Main for RC4 encryption,
      * @param message the message to encrypt
+     * @param seed the seed needed to encrypt or decrypt message seed size adapts to the needed size
      */
 
-    public static void Rc4Cipher(String message) {
+    public static String Rc4Encrypt(String message, String seed) {
 
         // seed may become a parameter
-
-        String seed = "thisisaseed";
 
         int[] mainTable = new int[256];
         int[] key = new int[256];
 
-        BigInteger tempt = Lfsr.run(seed, 50, Optional.of(256));
-        System.out.println(tempt);
+        BigInteger keyBigInteger = Lfsr.run(seed, 50, Optional.of(256));
 
-        String test = tempt.toString(2);
-        System.out.println(test);
-        System.out.println(test.charAt(10));
+        String keyString = keyBigInteger.toString(2);
 
         // fills key array with each lfsr bit
 
         for(int e = 0; e<256; e++) {
-            key[e] = Character.getNumericValue(test.charAt(e));
-            System.out.print(key[e]);
+            key[e] = Character.getNumericValue(keyString.charAt(e));
         }
 
         // fills mainTable from index 0 with value 0 to index 255 vith value 255
@@ -51,12 +51,55 @@ public class Rc4 {
         }
 
         // Encrypt & Decrypt
-        String text = message; // Replace with input from menu
+        String text = message;
         byte[] cryptedText = rc4EncryptDecrypt(text.getBytes(), mainTable.clone());
-        System.out.println("\nCiphertext: " + Arrays.toString(cryptedText));
+        return Arrays.toString(cryptedText);
+    }
 
-        byte[] decryptedText = rc4EncryptDecrypt(cryptedText, mainTable.clone());
-        System.out.println("Decrypted: " + new String(decryptedText));
+
+    /**
+     * Main for RC4 decryption,
+     * @param message the message to encrypt
+     * @param seed the seed needed to encrypt or decrypt message seed size adapts to the needed size
+     */
+
+     public static String Rc4Decrypt(String message, String seed) {
+
+        // seed may become a parameter
+
+        int[] mainTable = new int[256];
+        int[] key = new int[256];
+
+        BigInteger keyBigInteger = Lfsr.run(seed, 50, Optional.of(256));
+
+        String keyString = keyBigInteger.toString(2);
+
+        // fills key array with each lfsr bit
+
+        for(int e = 0; e<256; e++) {
+            key[e] = Character.getNumericValue(keyString.charAt(e));
+        }
+
+        // fills mainTable from index 0 with value 0 to index 255 vith value 255
+        
+        for (int i = 0; i < 256; i++) {
+            mainTable[i] = i;
+        }
+
+        // KSA
+
+        int j = 0;
+        for (int i = 0; i < 256; i++) {
+            j = (j + mainTable[i] + key[i]) % 256;
+            swap(mainTable, i, j);
+        }
+
+        String text = message;
+        byte[] cryptedText = rc4EncryptDecrypt(text.getBytes(), mainTable.clone());
+        
+    byte[] decryptedText = rc4EncryptDecrypt(cryptedText, mainTable.clone());
+    System.out.println("Decrypted: " + new String(decryptedText));
+        
     }
 
     // Encrypts from input using the maintable and the rc4 algorithm
