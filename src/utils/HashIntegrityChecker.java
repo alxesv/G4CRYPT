2512714fd95a4e233cb6fca6f1b6da523c4256b5
Léandre <leandre.colard@gmail.com>
2024-12-11 11:07:53 +0100

    WIP hashIntegrityChecker md5

--- src/utils/HashIntegrityChecker.java
+++ src/utils/HashIntegrityChecker.java
@@ -8,13 +8,17 @@ public class HashIntegrityChecker {
     }
     public void Checker(String method, String messageString, String existingMessageString) {
         if(method.equals("Md5")) {
-            Md5.formatter(existingMessageString);
-
-
+            String messageStringFormatted = Md5.formatter(messageString);
+            if(existingMessageString.equals(messageStringFormatted)) {
+                System.out.println("ok");
+            } else {
+                System.out.println("ko");
+            }
         } else if(method.equals("Sha256")) {
+            String messStringaStringFormatted = Sha256.formatter(messageString);
 
         } else {
-            System.out.println("Error method");
+            System.out.println("Method Error");
         }
         
     }
