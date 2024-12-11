969d64411cfeb9a648d7ae957179f9f99ddd797a
Léandre <leandre.colard@gmail.com>
2024-12-11 10:19:50 +0100

    creation of class

---
+++ src/utils/HashIntegrityChecker.java
@@ -0,0 +1,9 @@
+public class HashIntegrityChecker {
+    public static void main(String[] args) {
+        
+        
+    }
+    public void Checker(String method, String messageString, String existingMessageString) {
+        
+    }
+}
