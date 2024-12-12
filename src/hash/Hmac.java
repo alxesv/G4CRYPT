fd9cf2390df24ab9be6a076b9b9dd091a6686c64
Lucastor05 <95181838+Lucastor05@users.noreply.github.com>
2024-12-12 15:23:24 +0100

    Update Hmac.java

--- src/hash/Hmac.java
+++ src/hash/Hmac.java
@@ -13,19 +13,15 @@ public class Hmac {
      * @param input the password to hash
      * @return double hashed password
      */
-
     public static String encrypt(String input, String seed) {
 
-        // get pseudo random value from LFSR with a seed
-
+        // get pseudo random value from LFSR with a see
         String randFromSeed = Lfsr.run(seed, 50, Optional.of(256)).toString(16);
 
         // hash a first time with randFromCurrentTime as salt
-
         String firstHashString = Sha256.hashString(input.concat(randFromSeed));
 
         // hash a second time with PRIVATE_KEY_HMAC_STEP2 as pepper or private key
-
         String secondHashString = Sha256.hashString(firstHashString.concat(PRIVATE_KEY_HMAC_STEP2));
         
         return secondHashString;
