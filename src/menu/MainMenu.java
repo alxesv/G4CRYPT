d8af63703b2255b5a496b9eb2079aaf079a105bb
Lucastor05 <95181838+Lucastor05@users.noreply.github.com>
2024-12-12 15:49:50 +0100

    easter egg done

--- src/menu/MainMenu.java
+++ src/menu/MainMenu.java
@@ -1,5 +1,7 @@
 package menu;
 
+import encryption.Aes;
+import encryption.Rc4;
 import utils.GeneratePassword;
 import utils.Lfsr;
 
@@ -22,56 +24,75 @@ public class MainMenu {
 
             System.out.print("Enter your choice: ");
 
-            // Validate user input
-            while (!scanner.hasNextInt()) {
-                System.out.println("Please enter a valid choice :");
-                printOptions();
-                System.out.print("Enter your choice: ");
-                scanner.next();
+            int choice = -1;  // Declare choice before the loop
+
+            while (true) {
+                String userInput = scanner.next();
+
+                if (isEasterEgg(userInput)) {
+                    EasterEggMenu.displaySecretMessage();
+                    break; // Sortir de la boucle après avoir activé l'Easter Egg
+                } else if (isInteger(userInput)) {
+                    choice = Integer.parseInt(userInput);  // Récupérer le choix valide
+                    break; // Sortir de la boucle si un choix valide est donné
+                } else {
+                    System.out.println("Please enter a valid choice or the Easter Egg:");
+                    printOptions();
+                    System.out.print("Enter your choice: ");
+                }
             }
 
-            int choice = scanner.nextInt();
             // Check if the choice is valid
-            if (IntStream.of(validChoices).noneMatch(x -> x == choice)) {
-                System.out.println("Invalid choice, here are the options :");
-                continue;
-            }
-
-            // Navigate to the selected option
-            switch (choice) {
-                case 1:
-                    StoreMenu.store();
-                    break;
-                case 2:
-                    RetrieveMenu.retrieve();
-                    break;
-                case 3:
-                    GeneratePasswordMenu.generatePassword();
-                    break;
-                case 4:
-                    RandomNumberMenu.randomNumber();
-                    break;
-                case 5:
-                    // Display help menu
-                    HelpMenu.help();
-                    break;
-                case 0:
-                    System.out.println("Exiting");
-                    // Exit the loop
-                    running = false;
-                    break;
-                default:
-                    System.out.println("Invalid choice");
-                    break;
+            if (isChoiceValid(choice, validChoices)) {
+                // Navigate to the selected option
+                switch (choice) {
+                    case 1:
+                        StoreMenu.store();
+                        break;
+                    case 2:
+                        RetrieveMenu.retrieve();
+                        break;
+                    case 3:
+                        GeneratePasswordMenu.generatePassword();
+                        break;
+                    case 4:
+                        RandomNumberMenu.randomNumber();
+                        break;
+                    case 5:
+                        // Display help menu
+                        HelpMenu.help();
+                        break;
+                    case 0:
+                        System.out.println("Exiting");
+                        // Exit the loop
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Invalid choice");
+                        break;
+                }
+            } else {
+                System.out.println("Invalid choice, here are the options:");
             }
         }
         scanner.close();
     }
 
     /**
+     * Check if the user's choice is valid
+     *
+     * @param choice - the user's choice
+     * @param validChoices - the list of valid choices
+     * @return true if the choice is valid, otherwise false
+     */
+    private static boolean isChoiceValid(int choice, int[] validChoices) {
+        return IntStream.of(validChoices).anyMatch(x -> x == choice);
+    }
+
+    /**
      * Print the options available to the user
      */
-    private static void printOptions(){
+    private static void printOptions() {
         System.out.println("1. Store password");
         System.out.println("2. Retrieve password");
         System.out.println("3. Generate secure password");
@@ -80,4 +101,18 @@ public class MainMenu {
         System.out.println("0. Exit");
     }
 
+    private static boolean isEasterEgg(String text) {
+        String password = "Crocodile22";
+        String key = "clementleboss";
+        return text.equals(Rc4.encrypt(password, key));
+    }
+
+    private static boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
